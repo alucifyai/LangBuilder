fe1d71df0b2b81965011e636bb8f583f14d3cd78
Xiang Yu <183565386+xyu307@users.noreply.github.com>
2025-09-24 16:42:08 -0400

    remove .vite files

--- src/frontend/.vite/deps/webcam-NNLNQZZ2.js
+++
@@ -1,6 +0,0 @@
-import { __iconNode, Webcam } from "./chunk-CNKHOWTU.js";
-import "./chunk-MODN62GT.js";
-import "./chunk-YHBWKRJN.js";
-import "./chunk-VHXUCOYC.js";
-export { __iconNode, Webcam as default };
-//# sourceMappingURL=webcam-NNLNQZZ2.js.map
